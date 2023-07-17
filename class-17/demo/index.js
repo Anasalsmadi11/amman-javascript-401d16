2e0ad724fe4fde566b7b17e65db3cb6e1d414ed9
Anasalsmadi11 <anas.alsmadi411@gmail.com>
2023-07-17 21:27:29 +0000

    adding notes to class-17

--- class-17/demo/index.js
+++ class-17/demo/index.js
@@ -2,8 +2,8 @@
 // const AWS = require('aws-sdk');
 // const s3 = new AWS.S3();
 
-export const handler = async (event) => {
-    const MyResponse = JSON.stringify(event);
+export const handler = async (event) => { // this is how you define a lambda function
+    const MyResponse = JSON.stringify(event); // event is an object i want to convert it to JSON type once the function is invoked
     console.log('reading option is working ', MyResponse)
     //   // TODO implement
     //   const response = {
@@ -15,3 +15,17 @@ export const handler = async (event) => {
         body: `All is working \n ${MyResponse}`
     };
 };
+
+// example of converting an object to JSON string :
+
+// const person = {
+//     name: "John",
+//     age: 30,
+//     city: "New York"
+//   };
+  
+//   const jsonString = JSON.stringify(person);
+//   console.log(jsonString);
+
+//   // output
+//   {"name":"John","age":30,"city":"New York"}
