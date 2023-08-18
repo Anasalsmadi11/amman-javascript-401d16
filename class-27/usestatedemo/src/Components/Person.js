570b512daab3aa7a5289ecc8a1c8feb3b7e1cc7d
Anasalsmadi11 <anas.alsmadi411@gmail.com>
2023-08-18 17:17:21 +0000

    finished

--- class-27/usestatedemo/src/Components/Person.js
+++ class-27/usestatedemo/src/Components/Person.js
@@ -1,40 +1,19 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
 
-// function Person(props) {
-//     const [age, setAge] = useState(props.age || 36)
-//     const [name, setName] = useState(props.name || "shihab");
-//     const handleChange = (e) => {
-//         setName(e.target.value);
-//     }
-//     return (
-//         <>
-//             <h2 data-testid="name">My name is {name}</h2>
-//             <h3>My age is {age}</h3>
-
-//             <input data-testid="changedName" onChange={handleChange} />
-//         </>
-//     )
-// }
-
-// export default Person;
-
-import react ,{useState} from 'react'
-function Person(props){
-
-    const [name, setName]= useState(props.name || "kyoko")
-    const [age, setAge]= useState(props.age || "26")
-    
-    let handleChange=(e)=>{
-        setName(e.target.value)
-        console.log(e.target.value)
+function Person(props) {
+    const [age, setAge] = useState(props.age || 36)
+    const [name, setName] = useState(props.name || "shihab");
+    const handleChange = (e) => {
+        setName(e.target.value);
     }
     return (
         <>
-              <h2 >My name is {name}</h2>
-              <h3>My age is {age}</h3>
+            <h2 data-testid="name">My name is {name}</h2>
+            <h3>My age is {age}</h3>
 
-              <input onChange={handleChange}/>
+            <input data-testid="changedName" onChange={handleChange} />
         </>
     )
 }
-export default Person
+
+export default Person;
