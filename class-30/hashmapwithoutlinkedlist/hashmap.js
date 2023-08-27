3c75ea30b733347caa2e76d5373ee36008550bc0
Anasalsmadi11 <anas.alsmadi411@gmail.com>
2023-08-27 17:19:39 +0000

    finished

--- class-30/hashmapwithoutlinkedlist/hashmap.js
+++ class-30/hashmapwithoutlinkedlist/hashmap.js
@@ -1,33 +1,21 @@
+
+// if you want to see the result type in terminal node hashmap.js
 class Hashmap {
     constructor(size) {
         this.size = size;
-        this.map = new Array(size);
+        this.map = new Array(size); // this.map is the array i want to store the data in
     }
     // key="Cat" split ['C','a','t']
     makeHash(key) {
         const asciiCodeSum = key.split("").reduce((acc, char) => {
-            return acc + char.charCodeAt();
+            return acc + char.charCodeAt();  // charCodeAt is a built in that  gives you the number of the charecter
         }, 0);
-        const multiPrime = asciiCodeSum * 599;
+        const multiPrime = asciiCodeSum * 599; // 599 is a prime number , prime numbers are all the numbers greater than 1 and have no divisors other than 1 and themselves.  
         const theIndex = multiPrime % this.size;
-        return theIndex;
+        return theIndex;   // note that the index cannot be greater than the size of the array
     }
     add(key, value) {
         const hash = this.makeHash(key);
         this.map[hash] = { [key]: value };
     }
 }
-
-
-const myHashmap = new Hashmap(10);
-console.log(myHashmap.makeHash('ahmad'));
-console.log(myHashmap.makeHash('mohamad'));
-
-myHashmap.add("esam", "401d15 student");
-myHashmap.add("ahmad", "401d15 student");
-myHashmap.add("mohamad", "401d15 student");
-myHashmap.add("samah", "401d15 student");
-myHashmap.add("laith", "401d15 student");
-myHashmap.add("shihab", "401d15 student");
-
-// console.log(myHashmap);
